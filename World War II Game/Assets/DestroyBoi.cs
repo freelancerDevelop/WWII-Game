affc4e5a67ea9297dc4244a4281090d4c8c0fe51
Plutonium-Powered-Rocket <stanleyossyra@gmail.com>
2018-07-17 09:59:52 -0700

    Bomber Improved

---
+++ World War II Game/Assets/DestroyBoi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBoi : MonoBehaviour
+{
+    function Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            // toggle visibility:
+            renderer.enabled = !renderer.enabled;
+        }
+
+    }
+}
