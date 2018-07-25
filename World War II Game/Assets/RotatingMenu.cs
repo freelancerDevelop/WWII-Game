423f3f84e6c0aa158896aec704190ed84ff9cc79
Plutonium-Powered-Rocket <stanleyossyra@gmail.com>
2018-07-25 10:50:49 -0700

    DDAY DDAY DDAY DDAY DDAY DDAY DDAY DDAY

---
+++ World War II Game/Assets/RotatingMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingMenu : MonoBehaviour
+{
+    public float speed = 10f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.Rotate(Vector2.up, speed * Time.deltaTime);
+    }
+}
